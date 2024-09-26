593e9583018ff1c65390127e7e862d3589783967
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-09-26 17:59:51 -0400

    Added Prompt and Initialize the Game

---
+++ LAB02/lab2.js
@@ -0,0 +1,8 @@
+// Deep Biswas - 200554124
+// Import the prompt module
+const prompt = require('prompt');
+// Initialize the game
+console.log("Welcome to Rock, Paper, Scissors using Node!");
+console.log("You can exit the game anytime by using Ctrl+C");
+console.log("Please enter : R, P or S to play the game");
+
