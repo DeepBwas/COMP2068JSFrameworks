f6d8f5aebb53d030811188befc92b490e2eba813
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-09-26 18:00:47 -0400

    Added Validation Schema for user prompt.

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -5,4 +5,13 @@ const prompt = require('prompt');
 console.log("Welcome to Rock, Paper, Scissors using Node!");
 console.log("You can exit the game anytime by using Ctrl+C");
 console.log("Please enter : R, P or S to play the game");
-
+// Define the validation schema
+const validation = {
+    properties: {
+        Choice: {
+        pattern: /^[rpsRPS]$/,
+        message: "Choice must be R, P, or S",
+        required: true
+        }
+    }
+};
