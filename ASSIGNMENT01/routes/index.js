6ba240e650522f3dd0c0e39bee82272a376b16dc
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-10-16 22:09:04 -0400

    Added route, basic layout and nav

--- ASSIGNMENT01/routes/index.js
+++ ASSIGNMENT01/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Portfolio' });
+});
+
+/* GET about me page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET contact me page. */
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'Contact Me' });
 });
 
 module.exports = router;
