f8477a966c6861f359d50091c8a852fd6ebb6319
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-11-26 14:42:14 -0500

    PicsForge initial setup and header

---
+++ ASSIGNMENT02/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var fs = require('fs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Read the header template content
+const headerTemplate = fs.readFileSync(path.join(__dirname, 'views', 'header.hbs'), 'utf8');
+
+app.use('/', (req, res, next) => {
+  res.locals.header = headerTemplate;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
