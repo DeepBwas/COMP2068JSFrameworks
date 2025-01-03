de76dd9971c48da3b08b20bfc850503de5eb377d
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2025-01-03 12:06:44 -0500

    Removed cookie explicit config

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -56,13 +56,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000,
-      sameSite: 'lax'
-    }
+    saveUninitialized: false
   })
 );
 
